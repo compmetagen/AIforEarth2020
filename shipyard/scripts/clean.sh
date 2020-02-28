4b2d79a204e134fe5a8a4f072965d53bdd99a2d5
Davide Albanese <davide.albanese@gmail.com>
2020-02-28 22:18:00 +0100

    Fix clean.sh

--- shipyard/scripts/clean.sh
+++ shipyard/scripts/clean.sh
@@ -24,13 +24,13 @@ if [ -f "${1}_2.fastq" ]; then
         ktrim=r k=23 mink=11 hdist=1 tpe tbo interleaved=f threads=$THREADS
     rm ${1}_?.fastq
 
-    bbduk.sh
+    bbduk.sh \
         in1=noadapt_1.fastq in2=noadapt_2.fastq \
         out1=nocont_1.fastq out2=nocont_2.fastq ref=artifacts,phix \
         k=31 hdist=1 interleaved=f threads=$THREADS
     rm noadapt_?.fastq
 
-    bbduk.sh
+    bbduk.sh \
         in1=nocont_1.fastq in2=nocont_2.fastq \
         out1=${1}_1.fastq out2=${1}_2.fq \
         minavgquality=3 maxns=4 qtrim=r trimq=10 mlf=0.5 minlength=50 \
@@ -38,15 +38,17 @@ if [ -f "${1}_2.fastq" ]; then
     rm nocont_?.fastq
 else 
     # single-end
-    bbduk.sh in=${1}.fastq out=noadapt.fastq ref=adapters \
+    bbduk.sh \
+        in=${1}.fastq out=noadapt.fastq ref=adapters \
         ktrim=r k=23 mink=11 hdist=1 tpe tbo interleaved=f threads=$THREADS
     rm ${1}.fastq
 
-    bbduk.sh in=noadapt.fastq out=nocont.fastq ref=artifacts,phix \
+    bbduk.sh \
+        in=noadapt.fastq out=nocont.fastq ref=artifacts,phix \
         k=31 hdist=1 interleaved=f threads=$THREADS
     rm noadapt.fastq
 
-    bduk.sh
+    bduk.sh \
         in=nocont.fastq out=${1}.fastq \
         minavgquality=3 maxns=4 qtrim=r trimq=10 mlf=0.5 minlength=50 \
         interleaved=f threads=$THREADS
